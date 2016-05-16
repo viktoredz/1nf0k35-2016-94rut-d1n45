8329916a84cd907ca73f623da1092aa1336615a6
ginanjar <gin.bqs@gmail.com>
2016-05-16 14:58:57 +0700

    Revert "bhp permintaan"
    This reverts commit 453d2d59e7ef72518445b48259051839fdbd1e7d.

--- application/config/database.php
+++ application/config/database.php
@@ -5,7 +5,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = "triadpass";
 $db['default']['database'] = "epus_prog_3205";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
